ad187260affd7d23ee774e9e237dd157a4621590
denis875 <denisok578@gmail.com>
2024-05-28 20:51:07 +0300

    Практическая задача 3.1.4 Java pre-project. Задача 3.2. Работа с Bootstrap.

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,61 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService us;
+    @Autowired
+    private RoleService rs;
+
+    @GetMapping(value = "/admin")
+    public String startPageForAdmin(ModelMap model, @AuthenticationPrincipal UserDetails userDetail) {
+        User user = us.findByUsername(userDetail.getUsername());
+        model.addAttribute("curUser", user);
+        model.addAttribute("users", us.getAll());
+        model.addAttribute("roles", rs.getAllRoles());
+        model.addAttribute("newUser", new User());
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String showUserInfo(Model model, Principal principal) {
+        User user = us.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    @PostMapping("/admin/saveUser")
+    public String addUser(@ModelAttribute("newUser") User user) {
+        us.add(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/deleteUser")
+    public String deleteUser(@RequestParam Long id) {
+        us.delete(id);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/updateUser")
+    public String updateUserInfo(@ModelAttribute("user") User user) {
+        us.update(user);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,55 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getRole();
+    }
+}
